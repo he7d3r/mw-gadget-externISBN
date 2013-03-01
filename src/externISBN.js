5d26a10922324596526cc4cb419443eb0dadabac
Helder <he7d3r@gmail.com>
2013-03-01 19:46:25 +0000

    Small changes to validate with JSHint

--- src/externISBN.js
+++ src/externISBN.js
@@ -3,15 +3,23 @@
  * @author: [[w:en:User:Lunchboxhero]]
  * @tracking: [[Special:GlobalUsage/User:Helder.wiki/Tools/externISBN.js]] ([[File:User:Helder.wiki/Tools/externISBN.js]])
  */
+/*jshint laxbreak: true */
+/*global jQuery, mediaWiki */
+( function ( mw, $ ) {
+'use strict';
+
 if ( mw.config.get( 'wgCanonicalSpecialPageName' ) !== 'Booksources'
-	&& $.inArray(mw.config.get('wgPageName'), [ 'Wikilivros:Fontes_de_livros', 'Wikipedia:Book sources' ]) === -1 ) {
-	$(function () {
-		var	book_source_URL = '//www.google.com/search?tbs=bks:1&q=isbn:MAGICNUMBER',
+	&& $.inArray( mw.config.get('wgPageName'), [ 'Wikilivros:Fontes_de_livros', 'Wikipedia:Book sources' ] ) === -1
+) {
+	$( function () {
+		var bookSourceURL = '//www.google.com/search?tbs=bks:1&q=isbn:MAGICNUMBER',
 			magicRegex = /MAGICNUMBER/ig;
-		$('#content a').each(function(){
-			if ( this.href.match(/Special:BookSources\/(.*)/)) {
-				$(this).attr('href', book_source_URL.replace(magicRegex, RegExp.$1.replace(/[\D]*/g, '')) );
+		$( '#content' ).find( 'a' ).each( function(){
+			if ( this.href.match( /Special:BookSources\/(.*)/ ) ) {
+				$( this ).attr( 'href', bookSourceURL.replace( magicRegex, RegExp.$1.replace( /[\D]*/g, '' ) ) );
 			}
-		});
-	});
+		} );
+	} );
 }
+
+}( mediaWiki, jQuery ) );
