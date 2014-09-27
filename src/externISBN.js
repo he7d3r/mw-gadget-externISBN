c9222605455924ef983fde3dfdba6d0bf79a8fee
Helder <he7d3r@gmail.com>
2014-09-27 20:48:59 -0300

    Apply MediaWiki Coding Conventions
    Validated with jscs 1.6.2, using preset "wikimedia"

--- src/externISBN.js
+++ src/externISBN.js
@@ -3,20 +3,20 @@
  * @author: [[w:en:User:Lunchboxhero]]
  */
 ( function ( mw, $ ) {
-'use strict';
+	'use strict';
 
-if ( mw.config.get( 'wgCanonicalSpecialPageName' ) !== 'Booksources'
-	&& $.inArray( mw.config.get('wgPageName'), [ 'Wikilivros:Fontes_de_livros', 'Wikipedia:Book sources' ] ) === -1
-) {
-	$( function () {
-		var bookSourceURL = '//www.google.com/search?tbs=bks:1&q=isbn:MAGICNUMBER',
-			magicRegex = /MAGICNUMBER/ig;
-		$( '#content' ).find( 'a' ).each( function(){
-			if ( this.href.match( /Special:BookSources\/(.*)/ ) ) {
-				$( this ).attr( 'href', bookSourceURL.replace( magicRegex, RegExp.$1.replace( /[\D]*/g, '' ) ) );
-			}
+	if ( mw.config.get( 'wgCanonicalSpecialPageName' ) !== 'Booksources'
+		&& $.inArray( mw.config.get('wgPageName'), [ 'Wikilivros:Fontes_de_livros', 'Wikipedia:Book sources' ] ) === -1
+	) {
+		$( function () {
+			var bookSourceURL = '//www.google.com/search?tbs=bks:1&q=isbn:MAGICNUMBER',
+				magicRegex = /MAGICNUMBER/ig;
+			$( '#content' ).find( 'a' ).each( function () {
+				if ( this.href.match( /Special:BookSources\/(.*)/ ) ) {
+					$( this ).attr( 'href', bookSourceURL.replace( magicRegex, RegExp.$1.replace( /[\D]*/g, '' ) ) );
+				}
+			} );
 		} );
-	} );
-}
+	}
 
 }( mediaWiki, jQuery ) );
